39d992250a3c65ba4819fa9d6c172526431ca3f5
GaoShuai <gaoshuai@tuyougame.com>
2023-10-19 20:06:23 +0800

    feat: 新增mock文件

---
+++ mock/admin.ts
@@ -0,0 +1,89 @@
+import { Response } from "@/utils/axiosReq";
+import { IResponseRoles, IRoleItem } from "@/api/admin.pb";
+import { MockMethod } from "vite-plugin-mock";
+
+const roles: IRoleItem[] = [
+  {
+    id: 1,
+    name: "管理员",
+    create_time: new Date().getTime(),
+    create_user: "子嘉",
+    update_time: new Date().getTime(),
+    update_user: "子嘉"
+  },
+  {
+    id: 2,
+    name: "客服部门主管",
+    create_time: new Date().getTime(),
+    create_user: "子嘉",
+    update_time: new Date().getTime(),
+    update_user: "子嘉",
+    father_id: 1,
+    father_name: "管理员"
+  },
+  {
+    id: 3,
+    name: "客服部门组长",
+    create_time: new Date().getTime(),
+    create_user: "子嘉",
+    update_time: new Date().getTime(),
+    update_user: "子嘉",
+    father_id: 2,
+    father_name: "客服部门主管"
+  },
+  {
+    id: 4,
+    name: "客服部门成员",
+    create_time: new Date().getTime(),
+    create_user: "子嘉",
+    update_time: new Date().getTime(),
+    update_user: "子嘉",
+    father_id: 3,
+    father_name: "客服部门组长"
+  },
+  {
+    id: 5,
+    name: "销售部门主管",
+    create_time: new Date().getTime(),
+    create_user: "子嘉",
+    update_time: new Date().getTime(),
+    update_user: "子嘉",
+    father_id: 1,
+    father_name: "客服部门组长"
+  },
+  {
+    id: 6,
+    name: "销售部门组长",
+    create_time: new Date().getTime(),
+    create_user: "子嘉",
+    update_time: new Date().getTime(),
+    update_user: "子嘉",
+    father_id: 5,
+    father_name: "销售部门组长"
+  },
+  {
+    id: 7,
+    name: "运维部门主管",
+    create_time: new Date().getTime(),
+    create_user: "子嘉",
+    update_time: new Date().getTime(),
+    update_user: "子嘉",
+    father_id: 1,
+    father_name: "管理员"
+  }
+];
+export default [
+  {
+    url: "/v1/roles/",
+    method: "get",
+    response: (): Response<IResponseRoles> => {
+      return {
+        code: 0,
+        detail: "success",
+        data: {
+          data_list: roles
+        }
+      };
+    }
+  }
+] as MockMethod[];
